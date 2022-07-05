d45ae27c290df1143c469579f55ec236c1ff074b
Bernardo C. Franceschina <39697515+BernardoFranceschina@users.noreply.github.com>
2022-07-05 09:33:35 -0300

    Update main.py

--- Poker/main.py
+++ Poker/main.py
@@ -2,22 +2,48 @@ from Deck import *
 from Jogador import *
 
 jogadores = []
-jogadorSmall = 0
-jogadorBig = 1
-minimalBet = 5
+jogadorSmall = 1
+jogadorBig = 2
+smallBet = 5
+minimalBet = (smallBet * 2)
+# rodada = 1
 
 deck = Deck()
+apostas = []
+
+
+def bet():
+    for rodada in range(1, 3):
+        for jogador in jogadores:
+            while 
+            print('Rodada ', rodada, '- Vez do jogador numero', jogador.getNumeroJogador())
+            opcaoStr = ''
+            if rodada == 1:
+                if jogador.getNumeroJogador() == jogadorSmall:
+                    opcaoStr = '1) Fazer SMALL BET'
+                if jogador.getNumeroJogador() == jogadorBig:
+                    opcaoStr = '1) Fazer BIG BET'
+            opcao = int(input(opcaoStr))
+
+def validaJogadoresComFicha():
+    for jogador in jogadores:
+        if jogador.getFichas() < minimalBet:
+            print('Jogador ', jogador.getNumeroJogador(), 'não possui fichas suficientes para continuar jogando!')
+            # jogadores[jogador.getNumeroJogador()-1] = None
 
 
 def play():
-    deckEmbaralhado = deck.embaralharDeck()
-    smallbet = int(input('Small bet'))
-    bigbet = int(input('Big bet'))
-    apostas = [
-        {jogadores[jogadorSmall].getNumeroJogador(): smallbet},
-        {jogadores[jogadorBig].getNumeroJogador(): bigbet},
-    ]
-    print(apostas)
+    validaJogadoresComFicha()
+    distribuiCartas()
+    bet()
+
+
+def distribuiCartas():
+    deckEmbaralhado = deck.embaralhar()
+    for jogador in jogadores:
+        cartasDoJogador = deckEmbaralhado[:2]
+        deckEmbaralhado = deckEmbaralhado[2:]
+        jogador.setCartas(cartasDoJogador)
 
 
 def main():
